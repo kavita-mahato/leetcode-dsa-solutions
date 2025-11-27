0757a34281de49e78fb6cd610ad81feead08226b
Kavita Mahato <kavitamahato0835@gmail.com>
2025-11-27 18:24:57 +0530

    Check if Array is sorted and rotated

---
+++ S05_Arrays/checkSortedandRotated.java
@@ -0,0 +1,17 @@
+public class checkSortedandRotated {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+            // The array can have at most one drop
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
